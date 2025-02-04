a0aed3ff70d403db5b243d28f8573dceae0ac6b5
Liamell Cruz Disla <liamell_cruz@ucne.edu.do>
2025-02-03 19:40:42 -0500

    Se creo el contexto.

---
+++ Marcos_Rosario_P1_AP1/DAL/Contexto.cs
@@ -0,0 +1,13 @@
+﻿using Marcos_Rosario_P1_AP1.Models;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Options;
+
+namespace Marcos_Rosario_P1_AP1.DAL
+{
+    public class Contexto : DbContext
+    {
+        public Contexto(DbContextOptions<Contexto> options) : base(options) { }
+
+        public DbSet<Modelos> Modelos { get; set; }
+    }
+}
