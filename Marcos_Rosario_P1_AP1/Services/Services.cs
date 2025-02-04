91292f01faf2d74cfc7ff0c5fd6f40a915648245
Liamell Cruz Disla <liamell_cruz@ucne.edu.do>
2025-02-03 19:40:56 -0500

    Servicio creado.

---
+++ Marcos_Rosario_P1_AP1/Services/Services.cs
@@ -0,0 +1,45 @@
+﻿using Marcos_Rosario_P1_AP1.DAL;
+using Marcos_Rosario_P1_AP1.Models;
+using Microsoft.EntityFrameworkCore;
+using System.Linq.Expressions;
+
+namespace Marcos_Rosario_P1_AP1.Services
+{
+    public class Services(IDbContextFactory<Contexto> DbFactory)
+    {
+        public async Task<bool> Guardar(Modelos modelo)
+        {
+            return true;
+        }
+
+        private async Task<bool> Existe(int id)
+        {
+            return true;
+        }
+
+        private async Task<bool> Modificar(Modelos modelo)
+        {
+            return true;
+        }
+
+        private async Task<bool> Insertar(Modelos modelos)
+        {
+            return true;
+        }
+
+        public async Task<bool> Eliminar(int id)
+        {
+            return true;
+        }
+
+        public async Task<Modelos?> Buscar(int id)
+        {
+            return null;
+        }
+        
+        public async Task<List<Modelos>> Listar(Expression<Func<Modelos, bool>> criterio)
+        {
+            return null;
+        }
+    }
+}
