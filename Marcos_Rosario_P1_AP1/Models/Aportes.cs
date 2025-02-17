9210aa9006458ea2088ab60662d0e76249916961
Marcos Daniel Rosario Infante <marcos_rosario@ucne.edu.do>
2025-02-17 17:58:32 -0500

    Se modifico el modelo a modelo aportes.

---
+++ Marcos_Rosario_P1_AP1/Models/Aportes.cs
@@ -0,0 +1,20 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace Marcos_Rosario_P1_AP1.Models
+{
+    public class Aportes
+    {
+		[Key]
+        public int AporteId { get; set; }
+        [Required(ErrorMessage = "Campo Obligatorio.")]
+        [RegularExpression(@"^[a-zA-Z\s]+$", ErrorMessage = "Solo Caracteres Alfabeticos.")]
+        [StringLength(50, ErrorMessage = "Limite Excedido.")]
+        public string Name { get; set; }
+		[Required(ErrorMessage = "Campo Obligatorio.")]
+        [StringLength(100)]
+		public string Observacion {  get; set; }
+		[Required(ErrorMessage = "Campo Obligatorio.")]
+        [Range(0.01, 1000000, ErrorMessage = "Cantidad Excedida.")]
+		public double Monto { get; set; }
+	}
+}
