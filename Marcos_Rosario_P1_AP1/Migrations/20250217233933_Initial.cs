603c60c1d5f1cef233b85e8e5ed32537a421f046
Marcos Daniel Rosario Infante <marcos_rosario@ucne.edu.do>
2025-02-17 19:57:58 -0500

    Se creo la migracion de datos.

---
+++ Marcos_Rosario_P1_AP1/Migrations/20250217233933_Initial.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Marcos_Rosario_P1_AP1.Migrations
+{
+    /// <inheritdoc />
+    public partial class Initial : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "Aportes",
+                columns: table => new
+                {
+                    AporteId = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    Personas = table.Column<string>(type: "nvarchar(50)", maxLength: 50, nullable: false),
+                    Observacion = table.Column<string>(type: "nvarchar(100)", maxLength: 100, nullable: false),
+                    Monto = table.Column<double>(type: "float", nullable: false),
+                    Fecha = table.Column<DateTime>(type: "datetime2", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Aportes", x => x.AporteId);
+                });
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "Aportes");
+        }
+    }
+}
